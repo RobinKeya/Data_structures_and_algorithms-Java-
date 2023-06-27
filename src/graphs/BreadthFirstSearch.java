8bf1887be0722904a47770c00b644ef3b85fac42
RobinKeya <maimakrobin@gmail.com>
2023-06-27 13:00:04 +0300

    Breadth first search code

--- src/graphs/BreadthFirstSearch.java
+++ src/graphs/BreadthFirstSearch.java
@@ -1,2 +1,25 @@
-package graphs;public class BreadthFirstSearch {
+package graphs;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BreadthFirstSearch {
+    private void bfs(int startNode, List<List<Integer>> graph,int[]distance){
+        int INF = Integer.MAX_VALUE;
+        distance[startNode]=0;
+        Queue<Integer> queue= new LinkedList<>();
+        queue.add(startNode);
+
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+
+            for (int neighbour: graph.get(node)){
+                if (distance[neighbour]==INF){
+                    distance[neighbour]= distance[node]+1;
+                    bfs(neighbour,graph,distance);
+                }
+            }
+        }
+    }
 }
