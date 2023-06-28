ddc8159ff5c6d86d1f18a51c11cc887b2416a0c3
RobinKeya <maimakrobin@gmail.com>
2023-06-28 07:44:59 +0300

    Tree traversal code

--- src/common/TreeNode.java
+++ src/common/TreeNode.java
@@ -1,2 +1,10 @@
-package common;public class TreeNode {
+package common;
+
+public class TreeNode  {
+    public int data;
+    public TreeNode left, right;
+
+    public TreeNode(int data){
+        this.data = data;
+    }
 }
--- src/trees/TreeTraversal.java
+++ src/trees/TreeTraversal.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class TreeTraversal {
+package trees;
+
+import common.TreeNode;
+
+public class TreeTraversal {
+    private void inOrderTraversal(TreeNode root){
+        inOrderTraversal(root.left);
+        System.out.println(root.data);
+        inOrderTraversal(root.right);
+    }
+
+    private void preOrderTraversal(TreeNode root){
+        System.out.println(root.data);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+
+    private void postOrderTraversal(TreeNode root){
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.println(root.data);
+    }
 }
