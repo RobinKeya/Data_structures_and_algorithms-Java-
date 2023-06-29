72281093d66c8d6806f08d67d52121276a1673a0
RobinKeya <maimakrobin@gmail.com>
2023-06-29 22:40:18 +0300

    Solution to String compression problem.

--- src/CCI/arrays_and_strings/StringCompression.java
+++ src/CCI/arrays_and_strings/StringCompression.java
@@ -1,2 +1,23 @@
-package CCI.arrays_and_strings;public class StringCompression {
+package CCI.arrays_and_strings;
+
+public class StringCompression {
+    private static String compressString(String str){
+        StringBuilder results = new StringBuilder();
+        int count =1;
+        for (int i = 1; i< str.length(); i++){
+            if (str.charAt(i) == str.charAt(i-1)){
+                count++;
+            }else {
+                results.append(str.charAt(i - 1));
+                results.append(count);
+                count =1;
+            }
+        }
+        return results.toString().length() < str.length() ? results.toString() : str ;
+    }
+
+    public static void main(String[] args){
+        System.out.print(compressString("aabccccc"));
+    }
 }
+
