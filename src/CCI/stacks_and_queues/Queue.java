8c532066c027c83c2b26c3b3bf1cac617af26cca
RobinKeya <maimakrobin@gmail.com>
2023-06-30 11:17:41 +0300

    Queue code.

--- src/CCI/stacks_and_queues/Queue.java
+++ src/CCI/stacks_and_queues/Queue.java
@@ -1,2 +1,44 @@
-package CCI.stacks_and_queues;public class Queue {
+package CCI.stacks_and_queues;
+
+import java.util.NoSuchElementException;
+
+public class Queue<T> {
+    private class QueueNode<T>{
+        private T data;
+        private QueueNode<T> next;
+
+        public QueueNode(T data){
+            this.data = data;
+        }
+    }
+    private QueueNode<T> head;
+    private QueueNode<T> tail;
+
+    private boolean isEmpty(){
+        return head == null;
+    }
+    private T peek(){
+        if (head == null) throw new NoSuchElementException();
+        return head.data;
+    }
+
+    private void add(T item){
+        QueueNode t = new QueueNode(item);
+        if (tail != null){
+            tail.next = t;
+        }
+        tail = t;
+        if (head == null){
+            head = tail;
+        }
+    }
+    private T remove(){
+        if (head == null) throw new NoSuchElementException();
+        T item = head.data;
+        head = head.next;
+        if (head == null){
+            tail = null;
+        }
+        return item;
+    }
 }
