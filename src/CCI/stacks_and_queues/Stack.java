9f77a856481e7756b72e7c2514d0e19f80469f9c
RobinKeya <maimakrobin@gmail.com>
2023-06-30 11:17:24 +0300

    Stack code.

--- src/CCI/stacks_and_queues/Stack.java
+++ src/CCI/stacks_and_queues/Stack.java
@@ -1,2 +1,34 @@
-package CCI.stacks_and_queues;public class Stack {
+package CCI.stacks_and_queues;
+
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private class StackNode<T>{
+        T data;
+        StackNode<T> next;
+        public StackNode(T data){
+            this.data = data;
+        }
+    }
+    private StackNode<T> top;
+
+    private boolean isEmpty(){
+        return top == null;
+    }
+    private T peek(){
+        if (top == null) throw new EmptyStackException();
+        return top.data;
+    }
+    private void push(T item){
+        StackNode t = new StackNode(item);
+        t.next = top;
+        top = t;
+    }
+
+    private T pop(){
+        if (top== null) throw new EmptyStackException();
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
 }
