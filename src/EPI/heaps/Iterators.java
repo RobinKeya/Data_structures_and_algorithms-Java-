541b41069b2b3c2df7b91258897d1a73a7d49287
RobinKeya <maimakrobin@gmail.com>
2023-07-05 23:55:11 +0300

    Look into java iterators.

--- src/EPI/heaps/Iterators.java
+++ src/EPI/heaps/Iterators.java
@@ -1,2 +1,22 @@
-package EPI.heaps;public class Iterators {
+package EPI.heaps;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Iterators {
+    private static void iteratorTest(List<Integer> iter){
+        Iterator<Integer> result = iter.iterator();
+        while (result.hasNext()){
+            System.out.println(result.next());
+        }
+    }
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(5);
+        list.add(10);
+        iteratorTest(list);
+    }
 }
