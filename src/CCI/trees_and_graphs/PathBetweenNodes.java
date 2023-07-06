ac9e09ee841376918ab6cd496eea95a1c7003ce8
RobinKeya <maimakrobin@gmail.com>
2023-07-06 13:21:33 +0300

    Solution to find route between two nodes problem.

--- src/CCI/trees_and_graphs/PathBetweenNodes.java
+++ src/CCI/trees_and_graphs/PathBetweenNodes.java
@@ -1,2 +1,35 @@
-package CCI.trees_and_graphs;public class PathBetweenNodes {
+package CCI.trees_and_graphs;
+
+import common.Graph;
+import common.Node;
+import common.State;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PathBetweenNodes {
+    private static boolean findIfPathExists(Graph g, Node x, Node y){
+        Queue<Node> queue = new LinkedList<>();
+        for (Node u : g.nodes){
+            u.state = State.Unvisited;
+        }
+        queue.add(x);
+        x.state = State.Visiting;
+        while (!queue.isEmpty()){
+            Node u = queue.remove();
+            if (u != null){
+                for (Node neighbour: u.children){
+                    if (neighbour.state == State.Unvisited){
+                        if (neighbour.name == y.name)return  true;
+                        else {
+                            queue.add(neighbour);
+                            neighbour.state = State.Visiting;
+                        }
+                    }
+                }
+            }
+            u.state = State.Visited;
+        }
+        return false;
+    }
 }
