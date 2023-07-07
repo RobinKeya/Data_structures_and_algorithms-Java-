d97c014e26f4a89727d20ea4fe9bbdf8436fa184
RobinKeya <maimakrobin@gmail.com>
2023-07-07 23:30:55 +0300

    Code to sort Students according to gpa, tie-breaker being name.

--- src/EPI/searching/SortStudents.java
+++ src/EPI/searching/SortStudents.java
@@ -1,2 +1,28 @@
-package EPI.searching;public class SortStudents {
+package EPI.searching;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+// sort students according to there GPA and if the gpa ties, use name as tie-breaker.
+public class SortStudents {
+    private class Student{
+        public String name;
+        public double gradePointAverage;
+
+        public Student(String name, double gradePointAverage){
+            this.name = name;
+            this.gradePointAverage = gradePointAverage;
+        }
+    }
+    private static Comparator<Student> comparator = (o1, o2) -> {
+        if (o1.gradePointAverage != o2.gradePointAverage){
+            return Double.compare(o1.gradePointAverage , o2.gradePointAverage);
+        }
+        return o1.name.compareTo(o2.name);
+    };
+    private static boolean searchStudent(List<Student> students, Student target, Comparator<Student> comparator){
+        return Collections.binarySearch(students,target,comparator)>=0;
+    }
+
 }
